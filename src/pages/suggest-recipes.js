81fb7571291a3888fc93da5df65a93494a8c923f
Emma M. Neil <emmaneil722@gmail.com>
2020-07-02 20:12:54 -0400

    embedded google form

--- src/pages/suggest-recipes.js
+++ src/pages/suggest-recipes.js
@@ -1,19 +1,29 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import RecipeForm from "../components/RecipeForm"
 
 import "../components/RecipeForm/RecipeForm.css"
 
 const SuggestRecipesPage = ({ location }) => {
   return (
     <Layout className="site-text" location={location}>
-      <h2>Suggest Recipes</h2>
-      <p>
-        We welcome any and all recipe suggestions! Just fill out the following
-        form and we'll take a look.
-      </p>
-      <RecipeForm />
+      <div className="site-text">
+        <h2>Suggest Recipes</h2>
+        <p>
+          We welcome any and all recipe suggestions! Just fill out the following
+          form and we'll take a look.
+        </p>
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSdu6DgDnrNi8T9WKtTaOk6h_SyHiWjLg9DQdmaGj1sdXyuIFg/viewform?embedded=true"
+          width="640"
+          height="911"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+        >
+          Loading…
+        </iframe>
+      </div>
     </Layout>
   )
 }
