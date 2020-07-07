d210a160e92906f31f93d3fe02e11591fa9c5a2d
Emma M. Neil <emmaneil722@gmail.com>
2020-07-07 14:58:36 -0400

    added login back in

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Img from "gatsby-image"
 import Layout from "../components/layout"
+import Login from "../components/Login/identity"
 import SEO from "../components/seo"
 
 import "./styles.css"
@@ -13,40 +14,43 @@ const BlogIndex = ({ data, location }) => {
   const posts = data.allMarkdownRemark.edges
 
   return (
-    <Layout location={location} title={siteTitle}>
-      <SEO title="All posts" />
-      {posts.map(({ node }) => {
-        const title = node.frontmatter.title || node.fields.slug
-        return (
-          <div className="blog-previews" key={title}>
-            <Img
-              className="blog-post-image"
-              sizes={node.frontmatter.featuredImage.childImageSharp.sizes}
-            />
-            <article key={node.fields.slug}>
-              <div className="blog-preview-text">
-                <header>
-                  <h3 className="blog-post-title">
-                    <Link className="blog-link" to={node.fields.slug}>
-                      {title}
-                    </Link>
-                  </h3>
-                  <small>{node.frontmatter.date}</small>
-                </header>
-                <section>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: node.frontmatter.description || node.excerpt,
-                    }}
-                  />
-                </section>
-              </div>
-            </article>
-          </div>
-        )
-      })}
-      <Bio />
-    </Layout>
+    <div>
+      <Login />
+      <Layout location={location} title={siteTitle}>
+        <SEO title="All posts" />
+        {posts.map(({ node }) => {
+          const title = node.frontmatter.title || node.fields.slug
+          return (
+            <div className="blog-previews" key={title}>
+              <Img
+                className="blog-post-image"
+                sizes={node.frontmatter.featuredImage.childImageSharp.sizes}
+              />
+              <article key={node.fields.slug}>
+                <div className="blog-preview-text">
+                  <header>
+                    <h3 className="blog-post-title">
+                      <Link className="blog-link" to={node.fields.slug}>
+                        {title}
+                      </Link>
+                    </h3>
+                    <small>{node.frontmatter.date}</small>
+                  </header>
+                  <section>
+                    <p
+                      dangerouslySetInnerHTML={{
+                        __html: node.frontmatter.description || node.excerpt,
+                      }}
+                    />
+                  </section>
+                </div>
+              </article>
+            </div>
+          )
+        })}
+        <Bio />
+      </Layout>
+    </div>
   )
 }
 
